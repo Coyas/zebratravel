4d8ccd50b7d4cdb8d64b7e5f9e53a197738d14f9
Ailton Duarte <adidas.coyas@gmail.com>
2025-03-19 16:49:49 -0100

    fix: Error: Unexpected any

--- components/Team/TeamMember.tsx
+++ components/Team/TeamMember.tsx
@@ -1,13 +1,33 @@
 import React from "react";
+import Image from "next/image";
 
-const TeamMember = ({ name, image, designation }: any) => {
+// Definindo um tipo para as propriedades do TeamMember
+interface TeamMemberProps {
+	name: string;
+	image: string;
+	designation: string;
+}
+
+const TeamMember: React.FC<TeamMemberProps> = ({
+	name,
+	image,
+	designation,
+}) => {
 	return (
 		<div className="team-block-one">
 			<div className="inner-box">
 				<div className="image-box">
 					<div className="image">
 						<a href="#">
-							<img src={image} alt={name} title="" />
+							{/* Usando o componente Image do Next.js para otimização */}
+							<Image
+								src={image} // Caminho da imagem
+								alt={name} // Texto alternativo
+								title={name} // Título da imagem
+								width={300} // Largura da imagem
+								height={300} // Altura da imagem
+								layout="responsive" // Ajuste da imagem de forma responsiva
+							/>
 						</a>
 					</div>
 				</div>
@@ -19,7 +39,14 @@ const TeamMember = ({ name, image, designation }: any) => {
 					<div className="phone">
 						<a href="#" className="theme-btn call-btn">
 							<i className="icon">
-								<img src="images/icons/phone-call.svg" alt="" title="" />
+								{/* Usando Image também para o ícone */}
+								<Image
+									src="/images/icons/phone-call.svg"
+									alt="Phone icon"
+									title="Phone icon"
+									width={20} // Tamanho do ícone
+									height={20} // Tamanho do ícone
+								/>
 							</i>
 						</a>
 					</div>
--- components/Team/TeamSection.tsx
+++ components/Team/TeamSection.tsx
@@ -7,65 +7,65 @@ const TeamSection = () => {
 			id: 1,
 			name: "Andree Hoy",
 			designation: "Tour Guide",
-			image: "images/resource/team-1.jpg",
+			image: "/images/resource/team-1.jpg",
 		},
 		{
 			id: 2,
 			name: "Andree Hoy",
 			designation: "Tour Guide",
-			image: "images/resource/team-2.jpg",
+			image: "/images/resource/team-2.jpg",
 		},
 		{
 			id: 3,
 			name: "Andree Hoy",
 			designation: "Tour Guide",
-			image: "images/resource/team-3.jpg",
+			image: "/images/resource/team-3.jpg",
 		},
 		{
 			id: 4,
 			name: "Andree Hoy",
 			designation: "Tour Guide",
-			image: "images/resource/team-1.jpg",
+			image: "/images/resource/team-1.jpg",
 		},
 		{
 			id: 5,
 			name: "Andree Hoy",
 			designation: "Tour Guide",
-			image: "images/resource/team-2.jpg",
+			image: "/images/resource/team-2.jpg",
 		},
 		{
 			id: 6,
 			name: "Andree Hoy",
 			designation: "Tour Guide",
-			image: "images/resource/team-3.jpg",
+			image: "/images/resource/team-3.jpg",
 		},
 		{
 			id: 7,
 			name: "Andree Hoy",
 			designation: "Tour Guide",
-			image: "images/resource/team-1.jpg",
+			image: "/images/resource/team-1.jpg",
 		},
 		{
 			id: 8,
 			name: "Andree Hoy",
 			designation: "Tour Guide",
-			image: "images/resource/team-2.jpg",
+			image: "/images/resource/team-2.jpg",
 		},
 		{
 			id: 9,
 			name: "Andree Hoy",
 			designation: "Tour Guide",
-			image: "images/resource/team-3.jpg",
+			image: "/images/resource/team-3.jpg",
 		},
 	];
 
 	return (
 		<section className="team-section">
 			<div className="floated-icon left">
-				<img src="images/resource/team-icon-left.svg" alt="" title="" />
+				<img src="/images/resource/team-icon-left.svg" alt="" title="" />
 			</div>
 			<div className="floated-icon right">
-				<img src="images/resource/stones-right.svg" alt="" title="" />
+				<img src="/images/resource/stones-right.svg" alt="" title="" />
 			</div>
 			<div className="auto-container">
 				<div className="title-box centered">
