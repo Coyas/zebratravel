09e934ea7eca439b340467c6427577dbc5505038
Ailton Duarte <adidas.coyas@gmail.com>
2025-03-20 13:17:08 -0100

    feat: extract data from  products

--- components/Home/Products.tsx
+++ components/Home/Products.tsx
@@ -1,63 +1,9 @@
+// src/app/components/ProductSection.tsx
+
 "use client";
+
 import React from "react";
-
-interface Product {
-	imageUrl: string;
-	title: string;
-	price: string;
-	link: string;
-}
-
-const products: Product[] = [
-	{
-		imageUrl: "/images/resource/shop/prod-1.jpg",
-		title: "Smart Air Bag Travel",
-		price: "$225.00",
-		link: "product-single.html",
-	},
-	{
-		imageUrl: "/images/resource/shop/prod-2.jpg",
-		title: "Travel Suitcase Bag",
-		price: "$225.00",
-		link: "product-single.html",
-	},
-	{
-		imageUrl: "/images/resource/shop/prod-3.jpg",
-		title: "Travel Light Suitcase",
-		price: "$225.00",
-		link: "product-single.html",
-	},
-	{
-		imageUrl: "/images/resource/shop/prod-4.jpg",
-		title: "Smart Travel Gadgets",
-		price: "$225.00",
-		link: "product-single.html",
-	},
-	{
-		imageUrl: "/images/resource/shop/prod-5.jpg",
-		title: "Set of Black Silk Pillow",
-		price: "$225.00",
-		link: "product-single.html",
-	},
-	{
-		imageUrl: "/images/resource/shop/prod-6.jpg",
-		title: "Travel Slipping Support",
-		price: "$225.00",
-		link: "product-single.html",
-	},
-	{
-		imageUrl: "/images/resource/shop/prod-7.jpg",
-		title: "Travelling Black Suitcase",
-		price: "$225.00",
-		link: "product-single.html",
-	},
-	{
-		imageUrl: "/images/resource/shop/prod-8.jpg",
-		title: "Coffee Travel Mug",
-		price: "$225.00",
-		link: "product-single.html",
-	},
-];
+import { products } from "@/app/Dados/productsData"; // Importando os dados
 
 const ProductSection: React.FC = () => {
 	return (
