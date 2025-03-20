3bfc236de6716592e4dee3b8b7344f7a972f2770
Ailton Duarte <adidas.coyas@gmail.com>
2025-03-20 13:14:34 -0100

    feat: terms and conditions page

---
+++ app/terms-and-conditions/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { termsData } from "@/app/Dados/termos"; // Importa os dados simulados
+import InerBanner from "@/components/InerBanner";
+import bgImage from "@/public/images/background/banner-image-1.jpg";
+
+interface TermBlockProps {
+	title: string;
+	text: string;
+}
+
+const TermBlock: React.FC<TermBlockProps> = ({ title, text }) => {
+	return (
+		<div className="term-block">
+			<div className="inner">
+				<div className="title">
+					<h5>{title}</h5>
+				</div>
+				<div className="text">{text}</div>
+			</div>
+		</div>
+	);
+};
+
+const TermsSection: React.FC = () => {
+	const [terms, setTerms] = useState<any[]>([]);
+
+	// Simula a chamada da API
+	useEffect(() => {
+		// Aqui você pode substituir pela sua chamada de API
+		// Exemplo: fetch('sua-api-endpoint').then(response => response.json()).then(data => setTerms(data));
+
+		// Para este exemplo, vamos apenas usar os dados simulados
+		setTerms(termsData);
+	}, []); // O array vazio garante que isso aconteça apenas uma vez quando o componente for montado
+
+	return (
+		<>
+			<InerBanner backgroundImage={bgImage.src} />
+			<section className="terms-section">
+				<div className="auto-container">
+					<div className="content-box">
+						<div className="row clearfix">
+							<div className="text-col col-lg-6 col-md-12 col-sm-12">
+								{terms.slice(0, 4).map((term, index) => (
+									<TermBlock key={index} title={term.title} text={term.text} />
+								))}
+							</div>
+
+							<div className="text-col col-lg-6 col-md-12 col-sm-12">
+								{terms.slice(4).map((term, index) => (
+									<TermBlock key={index} title={term.title} text={term.text} />
+								))}
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default TermsSection;
