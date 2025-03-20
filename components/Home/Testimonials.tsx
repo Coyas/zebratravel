6ac9cf7a8a55c4dc3432b14f970b964e3418d166
Ailton Duarte <adidas.coyas@gmail.com>
2025-03-20 13:17:29 -0100

    feat: extract data from testimonials

--- components/Home/Testimonials.tsx
+++ components/Home/Testimonials.tsx
@@ -1,41 +1,9 @@
+// src/app/components/Testimonials.tsx
+
 "use client";
 
 import React from "react";
-
-interface Testimonial {
-	image: string;
-	text: string;
-	name: string;
-	designation: string;
-	rating: number;
-	backgroundImage?: string;
-	link?: string;
-}
-
-const testimonials: Testimonial[] = [
-	{
-		image: "/images/resource/testi-thumb-1.jpg",
-		text: "Fight School has specialized in martial arts since 1986 and has one of the most Fight School has specialized.",
-		name: "Randall Schwartz",
-		designation: "Women's Trainer",
-		rating: 5,
-	},
-	{
-		image: "/images/resource/testi-thumb-2.jpg",
-		text: "We teach martial arts because we love it — not because we want to make money on you. Unlike other martial.",
-		name: "Andru Smith",
-		designation: "Boxing Trainer",
-		rating: 5,
-	},
-	{
-		image: "/images/resource/testi-thumb-2.jpg",
-		text: "We teach martial arts because we love it — not because we want to make money on you. Unlike other martial.",
-		name: "Andru Smith",
-		designation: "Boxing Trainer",
-		rating: 5,
-	},
-	// Mais testemunhos podem ser adicionados aqui.
-];
+import { testimonials } from "@/app/Dados/testimonialsData"; // Importando os dados
 
 const Testimonials: React.FC = () => {
 	return (
