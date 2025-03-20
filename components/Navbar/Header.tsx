b74694e1a87cd77873737dd917e5e2239cf719d8
Ailton Duarte <adidas.coyas@gmail.com>
2025-03-20 13:18:09 -0100

    feat: extract data from header

--- components/Navbar/Header.tsx
+++ components/Navbar/Header.tsx
@@ -1,74 +1,7 @@
+// Header.tsx
 import React from "react";
 import Link from "next/link";
-
-// Definindo os tipos dos itens de menu
-interface MenuItem {
-	label: string;
-	href: string;
-	subMenu?: MenuItem[];
-}
-
-// Dados do menu
-const menuData: MenuItem[] = [
-	{
-		label: "Home",
-		href: "/",
-		// subMenu: [
-		// { label: "Home 01", href: "html/index.html" },
-		// { label: "Home 02", href: "html/index-2.html" },
-		// ],
-	},
-	{
-		label: "About Us",
-		href: "/about",
-		subMenu: [
-			// { label: "Our Team", href: "team.html" },
-			// { label: "Team Member", href: "team-member.html" },
-			// { label: "FAQs", href: "faq.html" },
-			{ label: "Galeria", href: "/galeria" },
-		],
-	},
-	{
-		label: "Loja",
-		href: "/loja",
-		// subMenu: [
-		// 	{ label: "Our Shop", href: "shop.html" },
-		// 	{ label: "Product Details", href: "product-single.html" },
-		// 	{ label: "My Wishlist", href: "wishlist.html" },
-		// 	{ label: "Shopping Cart", href: "shopping-cart.html" },
-		// 	{ label: "Checkout", href: "checkout.html" },
-		// 	{ label: "Signup", href: "signup.html" },
-		// 	{ label: "Login", href: "login.html" },
-		// 	{ label: "Forgot Password", href: "reset-password.html" },
-		// ],
-	},
-	// { label: "Destinations", href: "destinations.html" },
-	{ label: "Excursões", href: "/tours" },
-	{
-		label: "Pages",
-		href: "#",
-		// subMenu: [
-		// 	{ label: "Activities", href: "activities.html" },
-		// 	{ label: "Destinations 02", href: "destinations-2.html" },
-		// 	{ label: "Tours", href: "tours.html" },
-		// 	{ label: "Packages 01", href: "packages.html" },
-		// 	{ label: "Packages 02", href: "packages-2.html" },
-		// 	{ label: "Booking", href: "booking.html" },
-		// 	{ label: "Terms & Conditions", href: "terms-conditions.html" },
-		// 	{ label: "404 Page", href: "error-page.html" },
-		// ],
-	},
-	{
-		label: "Notiçias",
-		href: "/news",
-		// subMenu: [
-		// 	{ label: "Our Blog", href: "blog.html" },
-		// 	{ label: "Blog Classic", href: "blog-2.html" },
-		// 	{ label: "Blog Details", href: "blog-single.html" },
-		// ],
-	},
-	{ label: "Contato", href: "/contact" },
-];
+import { menuData } from "@/app/Dados/menu";
 
 const Header: React.FC = () => {
 	return (
@@ -81,7 +14,7 @@ const Header: React.FC = () => {
 						{/* Logo */}
 						<div className="logo-box">
 							<div className="logo">
-								<Link href="index.html" title="Treker">
+								<Link href="/" title="Treker">
 									<img src="/images/logo.svg" alt="Treker" title="Treker" />
 								</Link>
 							</div>
