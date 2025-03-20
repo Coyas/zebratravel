8c097a34653fbd23692cd2503ee8c5b84e89b6ad
Ailton Duarte <adidas.coyas@gmail.com>
2025-03-20 15:18:50 -0100

    feat: new shopping data

--- app/Dados/productsData.tsx
+++ app/Dados/productsData.tsx
@@ -1,59 +1,124 @@
 // src/app/Dados/productsData.ts
 
-export interface Product {
-	imageUrl: string;
-	title: string;
-	price: string;
+export interface Produto {
+	imagemUrl: string;
+	titulo: string;
+	preco: string;
 	link: string;
+	categoria: string; // Nova propriedade
 }
 
-export const products: Product[] = [
+export const productsData: Produto[] = [
 	{
-		imageUrl: "/images/resource/shop/prod-1.jpg",
-		title: "Smart Air Bag Travel",
-		price: "$225.00",
+		imagemUrl: "/images/resource/shop/prod-1.jpg",
+		titulo: "Mala de Viagem Smart Air Bag",
+		preco: "225,00€",
 		link: "product-single.html",
+		categoria: "Viagem", // Categoria
 	},
 	{
-		imageUrl: "/images/resource/shop/prod-2.jpg",
-		title: "Travel Suitcase Bag",
-		price: "$225.00",
+		imagemUrl: "/images/resource/shop/prod-2.jpg",
+		titulo: "Mala de Viagem com Roda",
+		preco: "225,00€",
 		link: "product-single.html",
+		categoria: "Viagem", // Categoria
 	},
 	{
-		imageUrl: "/images/resource/shop/prod-3.jpg",
-		title: "Travel Light Suitcase",
-		price: "$225.00",
+		imagemUrl: "/images/resource/shop/prod-3.jpg",
+		titulo: "Mala de Viagem Leve",
+		preco: "225,00€",
 		link: "product-single.html",
+		categoria: "Viagem", // Categoria
 	},
 	{
-		imageUrl: "/images/resource/shop/prod-4.jpg",
-		title: "Smart Travel Gadgets",
-		price: "$225.00",
+		imagemUrl: "/images/resource/shop/prod-4.jpg",
+		titulo: "Gadgets Smart para Viagem",
+		preco: "225,00€",
 		link: "product-single.html",
+		categoria: "Tecnologia", // Categoria
 	},
 	{
-		imageUrl: "/images/resource/shop/prod-5.jpg",
-		title: "Set of Black Silk Pillow",
-		price: "$225.00",
+		imagemUrl: "/images/resource/shop/prod-5.jpg",
+		titulo: "Conjunto de Almofadas de Seda Pretas",
+		preco: "225,00€",
 		link: "product-single.html",
+		categoria: "Acessórios", // Categoria
 	},
 	{
-		imageUrl: "/images/resource/shop/prod-6.jpg",
-		title: "Travel Slipping Support",
-		price: "$225.00",
+		imagemUrl: "/images/resource/shop/prod-6.jpg",
+		titulo: "Suporte para Dormir durante a Viagem",
+		preco: "225,00€",
 		link: "product-single.html",
+		categoria: "Viagem", // Categoria
 	},
 	{
-		imageUrl: "/images/resource/shop/prod-7.jpg",
-		title: "Travelling Black Suitcase",
-		price: "$225.00",
+		imagemUrl: "/images/resource/shop/prod-7.jpg",
+		titulo: "Mala de Viagem Preta",
+		preco: "225,00€",
 		link: "product-single.html",
+		categoria: "Viagem", // Categoria
 	},
 	{
-		imageUrl: "/images/resource/shop/prod-8.jpg",
-		title: "Coffee Travel Mug",
-		price: "$225.00",
+		imagemUrl: "/images/resource/shop/prod-8.jpg",
+		titulo: "Copo de Viagem para Café",
+		preco: "225,00€",
 		link: "product-single.html",
+		categoria: "Acessórios", // Categoria
+	},
+	{
+		imagemUrl: "/images/resource/shop/prod-1.jpg",
+		titulo: "Mala de Viagem Smart Air Bag eter",
+		preco: "225,00€",
+		link: "product-single.html",
+		categoria: "Viagem", // Categoria
+	},
+	{
+		imagemUrl: "/images/resource/shop/prod-2.jpg",
+		titulo: "Mala de Viagem com Rodaqrty",
+		preco: "225,00€",
+		link: "product-single.html",
+		categoria: "Viagem", // Categoria
+	},
+	{
+		imagemUrl: "/images/resource/shop/prod-3.jpg",
+		titulo: "Mala de Viagem Leve wwww",
+		preco: "225,00€",
+		link: "product-single.html",
+		categoria: "Viagem", // Categoria
+	},
+	{
+		imagemUrl: "/images/resource/shop/prod-4.jpg",
+		titulo: "Gadgets Smart para Viagem eeree",
+		preco: "225,00€",
+		link: "product-single.html",
+		categoria: "Tecnologia", // Categoria
+	},
+	{
+		imagemUrl: "/images/resource/shop/prod-5.jpg",
+		titulo: "Conjunto de Almofadas de Seda Pretas fgfgf",
+		preco: "225,00€",
+		link: "product-single.html",
+		categoria: "Acessórios", // Categoria
+	},
+	{
+		imagemUrl: "/images/resource/shop/prod-6.jpg",
+		titulo: "Suporte para Dormir durante a Viagem gfgf",
+		preco: "225,00€",
+		link: "product-single.html",
+		categoria: "Viagem", // Categoria
+	},
+	{
+		imagemUrl: "/images/resource/shop/prod-7.jpg",
+		titulo: "Mala de Viagem Preta ffff",
+		preco: "225,00€",
+		link: "product-single.html",
+		categoria: "Terra", // Categoria
+	},
+	{
+		imagemUrl: "/images/resource/shop/prod-8.jpg",
+		titulo: "Copo de Viagem para Café dfdf",
+		preco: "225,00€",
+		link: "product-single.html",
+		categoria: "System", // Categoria
 	},
 ];
