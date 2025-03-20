6b9dcaacf7881f4142982aaed011a4cf2a2465d3
Ailton Duarte <adidas.coyas@gmail.com>
2025-03-20 13:16:23 -0100

    feat: extrat data from qual selection func

--- components/Footer/qualSection.tsx
+++ components/Footer/qualSection.tsx
@@ -8,83 +8,21 @@ import { usePathname } from "next/navigation";
 import CartSidebar from "../CartSidebar";
 import { useState } from "react";
 
-const navItems = [
-	{
-		label: "Home",
-		href: "/",
-	},
-	{
-		label: "Sobre Nós",
-		href: "/about",
-	},
-	{
-		label: "Loja",
-		href: "/store",
-	},
-	{
-		label: "Expedições",
-		href: "/tours",
-	},
-	{
-		label: "Contato",
-		href: "/contact",
-	},
-];
+// Importando os dados do menu e do carrinho
+import { menuData } from "@/app/Dados/menu"; // Ajuste o caminho conforme sua estrutura de pastas
+import { cartItems } from "@/app/Dados/cartItems"; // Novo caminho para o arquivo cartItems.ts
 
 type SectionType = 0 | 1; // Tipando como número para os valores possíveis (0 ou 1)
 
-const QualSection = ({ sectionType }: { sectionType: SectionType }) => {
+interface QualSectionProps {
+	sectionType: SectionType;
+}
+
+const QualSection: React.FC<QualSectionProps> = ({ sectionType }) => {
 	const pathname = usePathname();
 
 	// Verifica se a rota atual é a página inicial
 	const isHomePage = pathname === "/";
-
-	type CartItem = {
-		id: number;
-		name: string;
-		price: number;
-		quantity: number;
-		imageUrl: string;
-	};
-
-	// Dados de exemplo para o carrinho
-	const cartItems: CartItem[] = [
-		{
-			id: 1,
-			name: "Smart Air Bag Travel",
-			price: 225.0,
-			quantity: 1,
-			imageUrl: "/images/resource/shop/shop-thumb-1.jpg",
-		},
-		{
-			id: 2,
-			name: "Smart Backpack",
-			price: 150.0,
-			quantity: 2,
-			imageUrl: "/images/resource/shop/shop-thumb-2.jpg",
-		},
-		{
-			id: 3,
-			name: "Smart Backpack",
-			price: 150.0,
-			quantity: 2,
-			imageUrl: "/images/resource/shop/shop-thumb-2.jpg",
-		},
-		{
-			id: 4,
-			name: "Smart Backpack",
-			price: 150.0,
-			quantity: 2,
-			imageUrl: "/images/resource/shop/shop-thumb-2.jpg",
-		},
-		{
-			id: 5,
-			name: "Smart Backpack",
-			price: 150.0,
-			quantity: 2,
-			imageUrl: "/images/resource/shop/shop-thumb-2.jpg",
-		},
-	];
 
 	// Calculando o subtotal
 	const subtotal = cartItems.reduce(
@@ -115,7 +53,7 @@ const QualSection = ({ sectionType }: { sectionType: SectionType }) => {
 							languageOptions={["PT", "EN", "Rus"]}
 							loginText="SIGN IN"
 							logoSrc="/images/logo.svg"
-							navItems={navItems}
+							navItems={menuData} // Usando os dados importados de menuData.ts
 						/>
 					)}
 					{/* Header é renderizado em todas as páginas, exceto na home */}
