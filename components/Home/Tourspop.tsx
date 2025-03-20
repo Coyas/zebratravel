216c39fcf4d98ac9bae1af3e51996e10cbaba7db
Ailton Duarte <adidas.coyas@gmail.com>
2025-03-20 13:18:00 -0100

    feat: extract data from tourspopulares

--- components/Home/Tourspop.tsx
+++ components/Home/Tourspop.tsx
@@ -1,102 +1,11 @@
+// src/app/components/PopularExcursions.tsx
+
 "use client";
 
 import React from "react";
+import { excursionData } from "@/app/Dados/excursionsData"; // Importando os dados
 
-const PopularExcursions = () => {
-	// Dados estáticos dentro do componente, adaptados para a Ilha do Fogo
-	const excursionData = [
-		{
-			id: 1,
-			title: "Pico do Fogo",
-			slug: "pico-do-fogo-vulcao",
-			image: {
-				url: "/images/resource/f-image-1.jpg", // Coloque o link da imagem do Pico do Fogo
-			},
-			price: 150,
-			days: 3,
-			location: "Pico do Fogo, Ilha do Fogo",
-			rating: 5,
-			reviews: 12,
-			description:
-				"Suba até o cume do vulcão ativo para uma vista incrível da ilha e das crateras ao redor.",
-		},
-		{
-			id: 2,
-			title: "Chã das Caldeiras",
-			slug: "cha-das-caldeiras",
-			image: {
-				url: "/images/resource/f-image-2.jpg", // Coloque o link da imagem de Chã das Caldeiras
-			},
-			price: 120,
-			days: 2,
-			location: "Chã das Caldeiras, Ilha do Fogo",
-			rating: 4,
-			reviews: 8,
-			description:
-				"Explore a vila dentro da cratera do vulcão, com paisagens únicas e vinhedos locais.",
-		},
-		{
-			id: 3,
-			title: "São Filipe",
-			slug: "sao-filipe-ilha-do-fogo",
-			image: {
-				url: "/images/resource/f-image-3.jpg", // Coloque o link da imagem de São Filipe
-			},
-			price: 100,
-			days: 1,
-			location: "São Filipe, Ilha do Fogo",
-			rating: 4,
-			reviews: 9,
-			description:
-				"Passeie pela charmosa cidade de São Filipe, com suas ruas históricas e vistas para o oceano.",
-		},
-		{
-			id: 4,
-			title: "Mosteiros",
-			slug: "mosteiros-ilha-do-fogo",
-			image: {
-				url: "/images/resource/f-image-4.jpg", // Coloque o link da imagem de Mosteiros
-			},
-			price: 130,
-			days: 2,
-			location: "Mosteiros, Ilha do Fogo",
-			rating: 4,
-			reviews: 7,
-			description:
-				"Visite os Mosteiros, com suas paisagens dramáticas e áreas perfeitas para caminhadas.",
-		},
-		{
-			id: 5,
-			title: "Praia de São Jorge",
-			slug: "praia-sao-jorge",
-			image: {
-				url: "/images/resource/f-image-5.jpg", // Coloque o link da imagem de Praia de São Jorge
-			},
-			price: 110,
-			days: 1,
-			location: "Praia de São Jorge, Ilha do Fogo",
-			rating: 5,
-			reviews: 15,
-			description:
-				"Relaxe na Praia de São Jorge, uma das praias mais bonitas da ilha, com águas cristalinas.",
-		},
-		{
-			id: 6,
-			title: "Vila de Patim",
-			slug: "vila-de-patim",
-			image: {
-				url: "/images/resource/f-image-5.jpg", // Coloque o link da imagem de Vila de Patim
-			},
-			price: 90,
-			days: 1,
-			location: "Vila de Patim, Ilha do Fogo",
-			rating: 4,
-			reviews: 6,
-			description:
-				"Descubra a tranquilidade da Vila de Patim, com suas belas paisagens rurais e cultura local.",
-		},
-	];
-
+const PopularExcursions: React.FC = () => {
 	return (
 		<section className="popular-section">
 			<div
