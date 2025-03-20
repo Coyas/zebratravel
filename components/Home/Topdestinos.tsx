7aa1d4418e4240e2a7c391dfb773b8b1607ce00d
Ailton Duarte <adidas.coyas@gmail.com>
2025-03-20 13:17:38 -0100

    feat: extract data from topdestinos

--- components/Home/Topdestinos.tsx
+++ components/Home/Topdestinos.tsx
@@ -1,48 +1,12 @@
+// src/app/components/TopDestinos.tsx
+
 "use client";
 
 import React from "react";
 import Image from "next/image";
+import { destinos } from "@/app/Dados/destinosData"; // Importando os dados
 
 const TopDestinos: React.FC = () => {
-	const destinos = [
-		{
-			nome: "Praia",
-			imagem: "/images/resource/gallery-1.jpg",
-			tours: 15,
-			colSize: "col-lg-8 col-md-12 col-sm-12",
-		},
-		{
-			nome: "Fogo",
-			imagem: "/images/resource/gallery-2.jpg",
-			tours: 15,
-			colSize: "column-width col-lg-4 col-md-6 col-sm-12",
-		},
-		{
-			nome: "Sal",
-			imagem: "/images/resource/gallery-3.jpg",
-			tours: 15,
-			colSize: "col-lg-6 col-md-6 col-sm-12",
-		},
-		{
-			nome: "São Vicente",
-			imagem: "/images/resource/gallery-4.jpg",
-			tours: 15,
-			colSize: "col-lg-6 col-md-6 col-sm-12",
-		},
-		{
-			nome: "Boa Vista",
-			imagem: "/images/resource/gallery-5.jpg",
-			tours: 15,
-			colSize: "column-width col-lg-4 col-md-6 col-sm-12",
-		},
-		{
-			nome: "Maio",
-			imagem: "/images/resource/gallery-6.jpg",
-			tours: 15,
-			colSize: "col-lg-8 col-md-12 col-sm-12",
-		},
-	];
-
 	return (
 		<section className="trending-destinations">
 			<div className="auto-container">
