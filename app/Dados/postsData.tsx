40e9342a3ee207d0d2d4b1d1e2172e82bd461191
Ailton Duarte <adidas.coyas@gmail.com>
2025-03-25 15:40:58 -0100

    fix: slug link

---
+++ app/Dados/postsData.tsx
@@ -0,0 +1,25 @@
+export const posts = [
+	{
+		id: 1,
+		title: "THE UPCOMING WBC CHAMP SHIPS 2022 in Feb",
+		author: "Sword Joy",
+		date: "20 March 2022",
+		image: "images/resource/news-1.jpg",
+		category: "Trekking",
+		description:
+			"Fight School has specialized in martial arts since 1986 and has one of the most innovative.",
+		slug: "sdf-3rr-erwfw3-sfsd-565dfs",
+	},
+	{
+		id: 2,
+		title: "THE BEST BOXERS IN THEIR WEIGHT CATEGORY",
+		author: "Sword Joy",
+		date: "20 March 2022",
+		image: "images/resource/news-2.jpg",
+		category: "Trekking",
+		description:
+			"Fight School has specialized in martial arts since 1986 and has one of the most innovative.",
+		slug: "sdf-3rr-erwfw3-sfsd-565dfs",
+	},
+	// Adicione mais posts conforme necessário
+];
