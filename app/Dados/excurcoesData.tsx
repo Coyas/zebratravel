6e78c166fd43bec4296a99399ec47b1ad6e0eab1
Ailton Duarte <adidas.coyas@gmail.com>
2025-03-27 12:16:12 -0100

    feat: add slug field

--- app/Dados/excurcoesData.tsx
+++ app/Dados/excurcoesData.tsx
@@ -1,4 +1,5 @@
 export type Excursao = {
+	slug: string;
 	title: string;
 	image: string;
 	price: number;
@@ -12,6 +13,7 @@ export type Excursao = {
 
 export const excursoesData: Excursao[] = [
 	{
+		slug: "ads-dada2-2edc-2323ds-asss",
 		title: "Moscow Red City Land",
 		image: "/images/resource/f-image-1.jpg",
 		price: 120,
@@ -24,6 +26,7 @@ export const excursoesData: Excursao[] = [
 		categories: ["easy-m", "popular"],
 	},
 	{
+		slug: "ads-dada2-2edc-2323ds-asss",
 		title: "Yellowstone & Mt Rushm",
 		image: "/images/resource/f-image-2.jpg",
 		price: 120,
@@ -36,6 +39,7 @@ export const excursoesData: Excursao[] = [
 		categories: ["moderate", "wild"],
 	},
 	{
+		slug: "ads-dada2-2edc-2323ds-asss",
 		title: "Los Angeles to San Franc",
 		image: "/images/resource/f-image-3.jpg",
 		price: 120,
@@ -48,6 +52,7 @@ export const excursoesData: Excursao[] = [
 		categories: ["m-difficult", "urban"],
 	},
 	{
+		slug: "ads-dada2-2edc-2323ds-asss",
 		title: "Best Of Switzerland",
 		image: "/images/resource/f-image-4.jpg",
 		price: 120,
@@ -60,7 +65,60 @@ export const excursoesData: Excursao[] = [
 		categories: ["difficult", "mountains"],
 	},
 	{
+		slug: "ads-dada2-2edc-2323ds-asss",
 		title: "Italian Dolomites",
+		image: "/images/resource/f-image-5.jpg",
+		price: 120,
+		duration: "5 days",
+		location: "259P, Himalaya Ag",
+		rating: 4,
+		reviews: 5,
+		description:
+			"Richly varied landscapes, luxurious accommodation and a wide range of wild experiences await you.",
+		categories: ["easy-m", "hiking"],
+	},
+	{
+		slug: "ads-dada2-2edc-2323ds-asss",
+		title: "Italian DolomitesAA",
+		image: "/images/resource/f-image-5.jpg",
+		price: 120,
+		duration: "5 days",
+		location: "259P, Himalaya Ag",
+		rating: 4,
+		reviews: 5,
+		description:
+			"Richly varied landscapes, luxurious accommodation and a wide range of wild experiences await you.",
+		categories: ["easy-m", "hiking"],
+	},
+	{
+		slug: "ads-dada2-2edc-2323ds-asss",
+		title: "Italian DolomitesQQQ",
+		image: "/images/resource/f-image-5.jpg",
+		price: 120,
+		duration: "5 days",
+		location: "259P, Himalaya Ag",
+		rating: 4,
+		reviews: 5,
+		description:
+			"Richly varied landscapes, luxurious accommodation and a wide range of wild experiences await you.",
+		categories: ["easy-m", "hiking"],
+	},
+	{
+		slug: "ads-dada2-2edc-2323ds-asss",
+		title: "Italian Dolomitesee",
+		image: "/images/resource/f-image-5.jpg",
+		price: 120,
+		duration: "5 days",
+		location: "259P, Himalaya Ag",
+		rating: 4,
+		reviews: 5,
+		description:
+			"Richly varied landscapes, luxurious accommodation and a wide range of wild experiences await you.",
+		categories: ["easy-m", "hiking"],
+	},
+	{
+		slug: "ads-dada2-2edc-2323ds-asss",
+		title: "Italian Dolomitesjj",
 		image: "/images/resource/f-image-5.jpg",
 		price: 120,
 		duration: "5 days",
